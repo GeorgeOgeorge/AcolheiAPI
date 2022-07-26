bab6b401b9e77464c7b1135fed46a7074e230a3a
Luan Luis Magioli <luanmagioli@gmail.com>
2022-07-26 09:57:31 -0300

    serializers

--- api/serializers.py
+++ api/serializers.py
@@ -83,7 +83,7 @@ class CardSerializer(serializers.ModelSerializer):
     def get_titulo(self, obj):
         elemento = ElementoComunicativo.objects.filter(id=obj.titulo_id).first()
         if(elemento != None):
-            return ElementoComunicativoSerializer(elemento)
+            return serializers.serialize('json', elemento.get_queryset())
 
     def get_descricao(self, obj):
         elemento = ElementoComunicativo.objects.filter(id=obj.descricao_id).first()
